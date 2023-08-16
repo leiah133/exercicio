3d0abefd98cd8e7971ddfdc0b19e519c43a0c3ee
leiah olive <liaanjos87@gmail.com>
2023-08-16 16:13:06 -0300

    Exercicio de fixação.

---
+++ src/ExercicioDiferenca.java
@@ -0,0 +1,34 @@
+
+/*
+Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Entrada: 5, 6, 7, 8
+Saída: DIFERENCA = -26
+-----------------------
+Entrada: 5 6 -7 8
+Saída: DIFERENCA = 86
+
+ */
+
+
+
+
+import java.util.Scanner;
+
+public class ExercicioDiferenca {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int dif = A * B - C * D;
+
+        System.out.println("DIFERENCA = " + dif);
+
+
+
+    }
+}
---
+++ src/ExercicioSoma.java
@@ -0,0 +1,30 @@
+/*
+Descrição do exercicio:
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+Exemplos:
+Entrada: 10, 30
+Saida: SOMA = 40
+---------------------
+Entrada: -30, 10
+ Saída: SOMA = -20
+ ------------------
+Entrada: 0, 0
+SOMA =  0
+ */
+
+import java.util.Scanner;
+
+public class ExercicioSoma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1= sc.nextInt();
+        int numero2 =  sc.nextInt();
+
+         int soma = numero1 + numero2;
+
+         System.out.println("Soma = " + soma);
+    sc.close();
+    }
+}
